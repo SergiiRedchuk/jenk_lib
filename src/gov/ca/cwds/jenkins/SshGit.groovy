7ce5f5fae10480373c76aa9ae7178b0f01f13f27
Sergii Redchuk <sergii.redchuk@osi.ca.gov>
2018-11-06 18:02:14 -0800

    1

---
+++ src/gov/ca/cwds/jenkins/SshGit.groovy
@@ -0,0 +1,14 @@
+package gov.ca.cwds.jenkins
+
+class SshGit {
+    def credentialsId
+    SshGit(credentialsId) {
+        this.credentialsId = credentialsId
+    }
+
+    def exec(String gitArgs) {
+        sshagent (credentials: [credentialsId]) {
+            sh(script: sshGitCommand(gitArgs), returnStatus: true)
+        }
+    }
+}
