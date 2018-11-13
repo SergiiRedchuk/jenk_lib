ea8ae44aed7c9282dd2bf39b06dc92c95cd12427
Sergii Redchuk <sergii.redchuk@osi.ca.gov>
2018-11-13 12:37:11 -0800

    2

--- src/gov/ca/cwds/jenkins/Utils.groovy
+++ src/gov/ca/cwds/jenkins/Utils.groovy
@@ -5,4 +5,12 @@ class Utils implements Serializable {
         // Used to avoid known_hosts addition, which would require each machine to have GitHub added in advance (maybe should do?)
         'GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" ' + command
     }
+
+    static boolean hasGradleBuildFile(script) {
+        script.sh (script: 'test -e build.gradle', returnStatus: true) == 0
+    }
+
+    static boolean hasPackageJsonFile(script) {
+        script.sh (script: 'test -e package.json', returnStatus: true) == 0
+    }
 }
