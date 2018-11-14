581d1033f6ee081e72d89ff48d46730c56550419
Sergii Redchuk <sergii.redchuk@osi.ca.gov>
2018-11-13 16:17:45 -0800

    2

--- src/gov/ca/cwds/jenkins/LicensingSupport.groovy
+++ src/gov/ca/cwds/jenkins/LicensingSupport.groovy
@@ -1,34 +1,56 @@
 package gov.ca.cwds.jenkins
 
 class LicensingSupport implements Serializable {
+    final def LICENSE_FOLDER = 'legal'
+
     def script
     def branchName
     def sshCredentialsId
+    def sshAgent
+    def licensingSupportType
 
     LicensingSupport(script, branchName, sshCredentialsId) {
         this.script = script
         this.branchName = branchName
         this.sshCredentialsId = sshCredentialsId
+        this.sshAgent = new SshAgent(script, sshCredentialsId)
+        this.licensingSupportType = getLicensingSupportType(script)
+        this.script.echo 'Detected Licensing Support Type: ' + this.licensingSupportType.title
     }
 
     def generateLicenseInfo() {
         if ('master' == this.branchName) {
-            def licensingSupportType = getLicensingSupportType(this.script)
-            this.script.echo 'Detected Licensing Support Type: ' + licensingSupportType.title
-            switch (licensingSupportType) {
+            switch (this.licensingSupportType) {
                 case LicensingSupportType.GRADLE_HIERYNOMUS_LICENSE:
-                    // todo deleteLicenses copyLicenses
-                    // todo no gradle wrapper?
                     this.script.sh './gradlew deleteLicenses downloadLicenses copyLicenses'
-                    // todo check result?
                     break
                 case LicensingSupportType.RUBY_LICENSE_FINDER:
+                    this.script.sh 'yarn licenses-report'
                     break
                 case LicensingSupportType.NONE:
                     throw new Exception('No known Licensing Support is found in the project')
             }
         } else {
-            script.echo 'Not working with the master branch. Skipping License Generation for an other branch.'
+            script.echo 'Not working with the master branch. Skipping License Generation for the other branch.'
+        }
+    }
+
+    def pushLicenseReport() {
+        if ('master' == this.branchName) {
+            switch (this.licensingSupportType) {
+                case LicensingSupportType.GRADLE_HIERYNOMUS_LICENSE:
+                case LicensingSupportType.RUBY_LICENSE_FINDER:
+                    this.sshAgent.exec('git config --global user.email cwdsdoeteam@osi.ca.gov')
+                    this.sshAgent.exec('git config --global user.name Jenkins')
+                    this.sshAgent.exec('git add ' + LICENSE_FOLDER)
+                    this.sshAgent.exec('git commit -m "updated license info"')
+                    this.sshAgent.exec('git push --set-upstream origin master')
+                    break
+                case LicensingSupportType.NONE:
+                    throw new Exception('No known Licensing Support is found in the project')
+            }
+        } else {
+            script.echo 'Not working with the master branch. Skipping Push License Report for the other branch.'
         }
     }
 
